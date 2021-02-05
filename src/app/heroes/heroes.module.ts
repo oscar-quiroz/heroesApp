d8925004428b9e3b17c9101f9f6605d371ab33ea
oscar-quiroz <kairox.rt@gmail.com>
2021-02-05 12:10:35 -0500

    rutas editar y ver

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,12 +10,13 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { MaterialModule } from '../material/material.module';
 import { HeroeTarjetaComponent } from './component/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
 
 @NgModule({
-  declarations: [AgregarComponent, BuscarComponent, HomeHeroesComponent, ListadoComponent, HeroeComponent,HeroeTarjetaComponent],
+  declarations: [AgregarComponent, BuscarComponent, HomeHeroesComponent, ListadoComponent, HeroeComponent,HeroeTarjetaComponent, ImagenPipe],
   imports: [
     CommonModule,
     MaterialModule,
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor( private  activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+  this.activatedRoute.params
+  .subscribe( console.log)
   }
 
 }
