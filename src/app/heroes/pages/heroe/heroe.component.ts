262b4def2ef9ba6208863da9dd32a96f69838044
oscar-quiroz <kairox.rt@gmail.com>
2021-02-05 12:47:52 -0500

    terminar listado de superheroes

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+import { switchMap } from 'rxjs/operators';
+import { ConstantPool } from '@angular/compiler';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
@@ -9,11 +14,24 @@ import { ActivatedRoute } from '@angular/router';
 export class HeroeComponent implements OnInit {
 
   
-  constructor( private  activatedRoute: ActivatedRoute) { }
+  heroe!:Heroe;
+
+  constructor( private  activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router:Router) { }
 
   ngOnInit(): void {
-  this.activatedRoute.params
-  .subscribe( console.log)
-  }
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroe(id)  )
+    )
+    .subscribe( heroe => this.heroe = heroe)
+ 
+   }
+
+   regresar(){
+     this.router.navigate(['/heroes/listado'])
+   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,13 +7,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule,MatListModule,MatCardModule
+    MatIconModule,MatListModule,MatCardModule,MatGridListModule,MatProgressSpinnerModule
   ],
   declarations: [],
   imports: [
